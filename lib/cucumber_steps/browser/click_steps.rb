eee14ab0eb7c899d24c0469bf93e0f17d21ccb50
Adam Luzsi <adamluzsi@gmail.com>
2015-10-20 18:21:58 +0200

    version bump , hover steps

--- lib/cucumber_steps/browser/click_steps.rb
+++ lib/cucumber_steps/browser/click_steps.rb
@@ -1,16 +1,18 @@
-
-And /^I click to the "([^"]+)" field that is near to the "([^"]+)" label$/i do |tag_name,label_text|
+And /^I click to the "([^"]+)" field that is near to the "([^"]+)" label$/i do |tag_name, label_text|
   relative_positioned_label = browser.current_scope.label(text: label_text).when_present
   element_to_click = relative_positioned_label.parent.element(tag_name: tag_name).when_present
+  element_to_click.hover rescue nil
   element_to_click.click
 end
 
 And /^I press the "([^"]+)" button$/i do |button_text|
   element_to_click = browser.current_scope.element(text: button_text).when_present
+  element_to_click.hover rescue nil
   element_to_click.click
 end
 
 And /^I press click to the element that html attributes match the following: (.*)$/ do |raw_html_attributes|
   element_to_click = browser.current_scope.element(parse_html_attributes(raw_html_attributes)).when_present
+  element_to_click.hover rescue nil
   element_to_click.click
 end
